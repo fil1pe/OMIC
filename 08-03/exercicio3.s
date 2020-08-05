d40edfb85ed7227d33c9ae5d0b4310c0731fcebf
Filipe Ramos <ik.ben.filipe@gmail.com>
2020-08-05 14:15:45 -0300

    Função pow separada em arquivo especial

--- 08-03/exercicio3.s
+++ 08-03/exercicio3.s
@@ -45,38 +45,6 @@ fim:
 	popq %rbp
 	ret
 
-# função que calcula potência de x^n
-    .text
-    .globl pow
-    .type power, @function
-pow:
-    # prólogo
-    pushq %rbp
-	movq %rsp, %rbp
-
-    movq $1, %rcx   # armazena o resultado
-pow_loop:
-    # se n <= 0, sai do loop
-    cmpl $0, %esi
-    jle pow_fim
-
-    # multiplica x pelo resultado anterior
-    movl %edi, %eax
-    mulq %rcx
-    movq %rax, %rcx
-
-    # decrementa n
-    decl %esi
-
-    jmp pow_loop
-pow_fim:
-    movq %rcx, %rax
-
-    # epílogo
-	movq %rbp, %rsp
-	popq %rbp
-	ret
-
     .section .rodata
 prompt:
     .string "Digite o número de coeficientes de p(x): "
